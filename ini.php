3c2ff3c2102c6ddffc668d1daa1390c5a32b83f2
Bernhard Rieder <berno.rieder@gmail.com>
2013-02-27 12:21:09 +0100

    initial commit

---
+++ ini.php
@@ -0,0 +1,15 @@
+<?php
+
+// +++++ twitter id and pass +++++
+
+$user = '';
+$pass = '';
+
+// +++++ database connection data +++++
+
+$hostname =	"localhost";					// define hostname
+$database =	"twittercapture"; 				// define database name
+$dbuser =	"root"; 						// user
+$dbpass =	"";
+	
+?>
